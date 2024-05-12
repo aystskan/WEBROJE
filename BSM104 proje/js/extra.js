2702f6f079eefa3fe16797f0ea96f576bb8874af
Ayşe TAŞKAN <152055077+asenasahovic@users.noreply.github.com>
2024-05-13 00:35:29 +0300

    Add files via upload
    12.05.2024
    12 MAY

--- BSM104 proje/js/extra.js
+++ BSM104 proje/js/extra.js
@@ -19,3 +19,10 @@ window.addEventListener("DOMContentLoaded",event=>{
         });
     }
 });
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for(let i=0;i<BtnCanvas.length;i++){
+    BtnCanvas[i].addEventListener("click",function()
+    {
+        document.querySelector('[data-bs-dismiss="offcanvas]').click();
+    });
+}
