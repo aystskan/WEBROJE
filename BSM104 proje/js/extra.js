3af866edccc3d95de451dd47b0532e489a431835
Ayşe TAŞKAN <152055077+asenasahovic@users.noreply.github.com>
2024-05-15 00:00:47 +0300

    Add files via upload
    14.05.2024
    May 14

--- BSM104 proje/js/extra.js
+++ BSM104 proje/js/extra.js
@@ -20,9 +20,40 @@ window.addEventListener("DOMContentLoaded",event=>{
     }
 });
 var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
-for(let i=0;i<BtnCanvas.length;i++){
+for(let i = 0; i < BtnCanvas.length; i++) {
     BtnCanvas[i].addEventListener("click",function()
     {
-        document.querySelector('[data-bs-dismiss="offcanvas]').click();
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
     });
 }
+
+(function () {
+  'use strict'
+ var myName = document.querySelector("#name");
+ var mySurame = document.querySelector("#surname");
+ var myEmail = document.querySelector("#email");
+ var myPhone = document.querySelector("#phone");
+ var myMessage = document.querySelector("#message");
+ var myBtn = document.querySelector("#BtnContact");
+
+ if(myMessage.value.length == 0) {
+    myBtn.disabled = 1;
+ }
+
+ myMessage.addEventListener("keyup", function () {
+    document.getElementById("current-character").textContent = myMessage.value.length;
+    if (myMessage.value.length >= 7) {
+        myBtn.disabled = 0;
+    } else {
+        myBtn.disabled = 1;
+    }
+ });
+//  var myForms =  document.querySelector(".doğrula");
+//  myForms.addEventListener("submit", function(event){
+//     if(!myForms.checkVolidity()) {
+//         e.preventDefault();
+//         e.stopPropagation();
+//     }
+//  }) form html hata denetlemeleri aktif tutulmasaydı kullanılacaktı 
+
+})();
