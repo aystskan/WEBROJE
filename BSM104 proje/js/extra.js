dfd23556a95aa57074175d649b467a193a7d262c
Ayşe TAŞKAN <152055077+asenasahovic@users.noreply.github.com>
2024-05-19 19:31:35 +0300

    Add files via upload

--- BSM104 proje/js/extra.js
+++ BSM104 proje/js/extra.js
@@ -27,37 +27,6 @@ for (let i = 0; i < BtnCanvas.length; i++) {
     });
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-
-
 (function () {
   'use strict'
  var myName = document.querySelector("#name");
@@ -304,3 +273,4 @@ $(document).ready(function() {
         }
     });
 });
+
